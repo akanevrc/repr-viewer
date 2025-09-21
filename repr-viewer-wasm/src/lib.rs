d5a34aaadc46b9ac04f250dcb09892c7e7747178
akanevrc <akanezoranomukou@gmail.com>
2025-09-21 16:43:52 +0900

    feat: 基本的な機能を追加

--- repr-viewer-wasm/src/lib.rs
+++ repr-viewer-wasm/src/lib.rs
@@ -1,9 +1,11 @@
 use bytemuck::NoUninit;
+#[cfg(target_endian = "little")]
+use bytemuck::Zeroable;
 use half::{bf16, f16};
 use std::str::FromStr;
 use wasm_bindgen::prelude::*;
 
-#[cfg(target_endian = "big")]
+#[cfg(target_endian = "little")]
 fn to_bytes<T: NoUninit + FromStr>(number: &str) -> Box<[u8]> {
     match number.parse::<T>() {
         Ok(value) => Vec::from(bytemuck::bytes_of(&value)).into_boxed_slice(),
@@ -11,7 +13,7 @@ fn to_bytes<T: NoUninit + FromStr>(number: &str) -> Box<[u8]> {
     }
 }
 
-#[cfg(target_endian = "little")]
+#[cfg(target_endian = "big")]
 fn to_bytes<T: NoUninit + FromStr>(number: &str) -> Box<[u8]> {
     match number.parse::<T>() {
         Ok(value) => bytemuck::bytes_of(&value)
@@ -24,7 +26,14 @@ fn to_bytes<T: NoUninit + FromStr>(number: &str) -> Box<[u8]> {
     }
 }
 
-macro_rules! def_fn {
+fn to_value<T: NoUninit + Zeroable + FromStr>(number: &str) -> T {
+    match number.parse::<T>() {
+        Ok(value) => value,
+        Err(_) => T::zeroed(),
+    }
+}
+
+macro_rules! def_to_bytes {
     ($fn_name:ident, $target_ty:ty) => {
         #[wasm_bindgen]
         pub fn $fn_name(number: &str) -> Box<[u8]> {
@@ -33,15 +42,35 @@ macro_rules! def_fn {
     };
 }
 
-def_fn!(to_u8, u8);
-def_fn!(to_u16, u16);
-def_fn!(to_u32, u32);
-def_fn!(to_u64, u64);
-def_fn!(to_i8, i8);
-def_fn!(to_i16, i16);
-def_fn!(to_i32, i32);
-def_fn!(to_i64, i64);
-def_fn!(to_f16, f16);
-def_fn!(to_bf16, bf16);
-def_fn!(to_f32, f32);
-def_fn!(to_f64, f64);
+macro_rules! def_to_value {
+    ($fn_name:ident, $target_ty:ty) => {
+        #[wasm_bindgen]
+        pub fn $fn_name(number: &str) -> $target_ty {
+            to_value::<$target_ty>(number)
+        }
+    };
+}
+
+def_to_bytes!(to_u8_bytes, u8);
+def_to_bytes!(to_u16_bytes, u16);
+def_to_bytes!(to_u32_bytes, u32);
+def_to_bytes!(to_u64_bytes, u64);
+def_to_bytes!(to_i8_bytes, i8);
+def_to_bytes!(to_i16_bytes, i16);
+def_to_bytes!(to_i32_bytes, i32);
+def_to_bytes!(to_i64_bytes, i64);
+def_to_bytes!(to_f16_bytes, f16);
+def_to_bytes!(to_bf16_bytes, bf16);
+def_to_bytes!(to_f32_bytes, f32);
+def_to_bytes!(to_f64_bytes, f64);
+
+def_to_value!(to_u8_value, u8);
+def_to_value!(to_u16_value, u16);
+def_to_value!(to_u32_value, u32);
+def_to_value!(to_u64_value, u64);
+def_to_value!(to_i8_value, i8);
+def_to_value!(to_i16_value, i16);
+def_to_value!(to_i32_value, i32);
+def_to_value!(to_i64_value, i64);
+def_to_value!(to_f32_value, f32);
+def_to_value!(to_f64_value, f64);
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,54 @@
 <script lang="ts">
-  import * as wasm from "repr-viewer-wasm";
-  let bytes = wasm.to_i32("-1");
+  import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text";
+  import Select, { Option } from "@smui/select";
+  import { type Ty, tys, isFloat, toSizes } from "./lib/ty";
+  import { toBytes, toValue } from "./lib/wasm_bridge";
+  import { toRepr, toReprSep } from "./lib/toRepr";
+
+  let number = $state("0");
+  let ty: Ty = $state("u8");
+  let sizes = $derived(toSizes(ty));
+  let bytes = $derived(toBytes(number, ty));
+  let repr = $derived(toRepr(bytes));
+  let reprSep = $derived(toReprSep(bytes, sizes));
+  let converted = $derived(toValue(number, ty));
+  let sign = $derived(reprSep && reprSep[0]);
+  let exp = $derived(reprSep && reprSep[1]);
+  let frac = $derived(reprSep && reprSep[2]);
 </script>
 
 <main>
-  <p>{bytes}</p>
+  <h1>Number Representation Viewer</h1>
+  <div>
+    <p>{repr ?? "変換できません"}</p>
+  </div>
+  <div>
+    <Select bind:value={ty} label="データ型">
+      {#each tys as t}
+        <Option value={t}>{t}</Option>
+      {/each}
+    </Select>
+    <Textfield bind:value={number} label="数値" required>
+      {#snippet helper()}
+        <HelperText>変換対象の数値を入力してください</HelperText>
+      {/snippet}
+    </Textfield>
+  </div>
+  {#if isFloat(ty)}
+    <div>
+      <Textfield bind:value={converted} label="変換後" />
+    </div>
+    <div>
+      <Textfield bind:value={sign} label="符号部" />
+    </div>
+    <div>
+      <Textfield bind:value={exp} label="指数部" />
+    </div>
+    <div>
+      <Textfield bind:value={frac} label="仮数部" />
+    </div>
+  {/if}
 </main>
 
 <style>
